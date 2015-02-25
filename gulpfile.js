b5605cb21271bc64e4b0be09c1338b6e0cc16c9f
Rickard <rickard.laurin@gmail.com>
2015-02-25 18:15:22 +0100

    Added SCSS, SCSS-linting and compiling in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,34 @@
-var gulp       = require('gulp');
-var browserify = require('gulp-browserify');
-var concat     = require('gulp-concat');
-var webserver  = require('gulp-webserver');
-var uglify     = require('gulp-uglify');
+var gulp         = require('gulp');
+var browserify   = require('gulp-browserify');
+var concat       = require('gulp-concat');
+var webserver    = require('gulp-webserver');
+var uglify       = require('gulp-uglify');
+var sass         = require('gulp-sass');
+var scsslint     = require('gulp-scss-lint');
+var autoprefixer = require('gulp-autoprefixer');
+var gutil        = require('gulp-util');
+var plumber      = require('gulp-plumber');
+var colors       = gutil.colors;
+
+
+//
+// Reporters
+// --------------------------------------------------
+var myCustomReporter = function(file) {
+  if (!file.scsslint.success) {
+    var filename = file.path.replace(process.cwd(), '');
+    gutil.log(file.scsslint.issues.length + ' issues found in ' + filename);
+
+    file.scsslint.issues.forEach(function (issue) {
+      var severity = issue.severity === 'warning' ? 'W' : 'E';
+      var error =  severity === 'E' ? colors.red(' [' + severity + '] ') : colors.yellow(' [' + severity + '] ');
+
+      var logMsg = colors.cyan(filename) + ':' + colors.magenta(issue.line) + error + issue.reason;
+
+      gutil.log(logMsg);
+    });
+  }
+};
 
 gulp.task('webserver', ['browserify', 'copy'], function() {
   gulp.src('dist')
@@ -24,9 +50,38 @@ gulp.task('copy', function() {
       .pipe(gulp.dest('dist'));
 });
 
-gulp.task('default',['browserify', 'copy', 'webserver']);
+//
+// SCSS
+// --------------------------------------------------
+gulp.task('scss-lint', function () {
+  gulp.src([
+      'app/scss/**/*.scss',
+      '!app/scss/vendor/**/*.scss'
+    ])
+    .pipe(scsslint({
+      config: './app/scss/scsslint.yml',
+      customReport: myCustomReporter,
+      maxBuffer: 300000 * 1024
+    }))
+});
+
+gulp.task('scss', function () {
+  gulp.src('./app/scss/screen.scss')
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions']
+    }))
+    .pipe(gulp.dest('./dist/css/'));
+});
+
+//
+// Tasks
+// --------------------------------------------------
+gulp.task('default',['browserify', 'copy', 'webserver', 'scss-lint', 'scss']);
 
 gulp.task('watch', function () {
+  gulp.watch(['./app/scss/**/*.scss'], ['scss-lint', 'scss']);
   gulp.watch(['./app/js/**/*.jsx', './app/js/**/*.js'], ['browserify']);
   gulp.watch(['./app/**/*.html'], ['copy']);
 })
