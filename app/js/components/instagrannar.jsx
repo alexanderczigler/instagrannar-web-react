dacbc5f690bfb4c14c4a30f290a15070901aa7f6
ilix <me@ilix.se>
2015-02-25 18:35:08 +0100

    Merge fixes + new url structure

--- app/js/components/instagrannar.jsx
+++ app/js/components/instagrannar.jsx
@@ -4,6 +4,7 @@ var React = require('react');
 var Header = require('./header.jsx');
 var Map = require('./map.jsx');
 var Footer = require('./footer.jsx');
+var Posts = require('./posts');
 
 module.exports = React.createClass({
   render: function () {
@@ -11,6 +12,7 @@ module.exports = React.createClass({
       <div>
         <Header />
         <Map />
+        <Posts url="http://instagrannar.se:3000/pictures?lng=18.119175068725554&lat=59.33500399999999&dst=350&max_ts=&min_ts=/-"/>
         <Footer />
       </div>
     );
--- app/js/main.js
+++ app/js/main.js
@@ -1,16 +1,9 @@
 /** @jsx React.DOM */
 
 var React = require('react');
-<<<<<<< HEAD
+
 var Instagrannar = require('./components/instagrannar.jsx');
 
 window.React = React; 
 
 React.render(<Instagrannar/>, document.body);
-=======
-var Posts = require('./components/posts');
-
-window.React = React; 
-
-React.render(<Posts url="http://localhost:3000/pictures/18.07277796549827/59.33369110415745/350/-"/>, document.body);
->>>>>>> 2fd62b1f68cce7e8996744ee637bd3c5633a6229
