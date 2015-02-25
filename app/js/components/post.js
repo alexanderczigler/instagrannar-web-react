34781816f1a3ea616ada1887e9e142bbbde0e20e
Rickard <rickard.laurin@gmail.com>
2015-02-25 20:36:09 +0100

    tweaked hover animation, added black box for instagrannar ad, corrected gulp task for scss-linting

--- app/js/components/post.js
+++ app/js/components/post.js
@@ -1,14 +1,22 @@
 /** @jsx React.DOM */
 
 var React = require('react');
+var addons = require('react-addons');
 
 module.exports = React.createClass({
   render: function () {
+    var cx = addons.classSet;
+    var classes = cx({
+      'posts__post col-4': true,
+      'posts__post--instagrannar': this.props.post.user.username === 'instagrannar'
+    });
+
     var user = '@' + this.props.post.user.username;
+    var image = this.props.post.user.username !== 'instagrannar' ? this.props.post.images.standard_resolution.url : '';
 
     return (
-      <div className="posts__post col-4" data-user={user}>
-        <img className="posts__image" src={this.props.post.images.standard_resolution.url} />
+      <div className={classes} data-user={user}>
+        <img className="posts__image" src={image} />
       </div>
     );
   }
