16c818daa5729b4931baf95007fe962316373259
ilix <me@ilix.se>
2015-02-25 21:30:30 +0100

    added google map

--- app/js/components/map.js
+++ app/js/components/map.js
@@ -3,9 +3,29 @@
 var React = require('react');
 
 module.exports = React.createClass({
+  getDefaultProps: function () {
+      return {
+          initialZoom: 8,
+          mapCenterLat: 43.6425569,
+          mapCenterLng: -79.4073126,
+      };
+  },
+  componentDidMount: function (rootNode) {
+      var mapOptions = {
+          center: this.mapCenterLatLng(),
+          zoom: this.props.initialZoom
+      },
+      map = new google.maps.Map(this.getDOMNode(), mapOptions);
+      var marker = new google.maps.Marker({position: this.mapCenterLatLng(), title: 'Hi', map: map});
+      this.setState({map: map});
+  },
+  mapCenterLatLng: function () {
+      var props = this.props;
+      return new google.maps.LatLng(props.mapCenterLat, props.mapCenterLng);
+  },
   render: function () {
-    return (
-      <div>Google map.</div>
-    );
+      return (
+        <div className='instagrannar-map'></div>
+      );
   }
 });
