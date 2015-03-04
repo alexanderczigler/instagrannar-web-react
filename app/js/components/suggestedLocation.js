0a3787703ae1ff2678492ab31cef3c9bc58c41ae
ilix <me@ilix.se>
2015-03-04 20:38:03 +0100

    Made the location suggester update to a new location when clicked.

--- app/js/components/suggestedLocation.js
+++ app/js/components/suggestedLocation.js
@@ -6,16 +6,23 @@ var LocationActions        = require('../actions/LocationActions');
 var PostStore              = require('../stores/PostStore');
 
 module.exports = React.createClass({
+  getInitialState: function () {
+    return {
+      location: SuggestedLocationStore.get()
+    };
+  },
   getByLocation: function (location) {
     LocationActions.getByLocation(location);
+    console.log('update');
+    this.setState({
+      location: SuggestedLocationStore.get()
+    });
   },
   render: function () {
-  	var location = SuggestedLocationStore.get();
-
-    return (
+  	return (
       <div className="location">
-        <a onClick={this.getByLocation.bind(this, location)}>
-          Slut på idéer? Spana in vad som händer i {location.name}
+        <a onClick={this.getByLocation.bind(this, this.state.location)}>
+          Slut på idéer? Spana in vad som händer i {this.state.location.name}
         </a>
       </div>
     );
